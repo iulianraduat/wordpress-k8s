b4ec4a56887278e246f08f5135ac7a21d2d9fae8
Iulian Radu <iulian.radu@gmx.at>
2024-02-04 17:28:06 +0100

    Update install-sshd-in-pod.sh
    Moved the change of ssh config before service start

--- install-sshd-in-pod.sh
+++ install-sshd-in-pod.sh
@@ -2,9 +2,9 @@
 POD=$(kubectl get pods|grep wordpress|grep -v mysql|cut -d ' ' -f1)
 kubectl exec $POD -- apt-get update
 kubectl exec $POD -- apt-get -y install openssh-server
+kubectl exec $POD -- sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 kubectl exec $POD -- service ssh restart
 kubectl exec $POD -- systemctl enable ssh
-kubectl exec $POD -- sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 kubectl exec $POD -- passwd root
 PWD=toor
 kubectl exec $POD -- echo -e "$PWD\n$PWD" | passwd root
